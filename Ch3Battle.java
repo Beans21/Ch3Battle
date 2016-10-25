f318006d081804f39af0e5598955a88fcecb2dc7
Beans21 <SKardashian19@windwardschool.org>
2016-10-25 15:27:52 -0700

    Update Ch3Battle.java

--- Ch3Battle.java
+++ Ch3Battle.java
@@ -1,8 +1,24 @@
 import java.util.*;
-public class Ch3Battle {
+public class Battle {
 
 	public static void main (String[] args) {
 		battleStart();
+		Scanner s= new Scanner(System.in);
+		System.out.println("Trainer, what are your Pokemon's stats?");
+		System.out.print("Level: ");
+		int level= s.nextInt();
+		System.out.print("Attack: ");
+		int attack= s.nextInt();
+		System.out.print("Defense: ");
+		int defense= s.nextInt();
+		System.out.print("Base: ");
+		int base= s.nextInt();
+		System.out.print("HP: ");
+		int hp= s.nextInt();
+		damage(level, attack, defense, base, hp);
+		System.out.print("Moves Learned: ");
+		String moveslearned= s.nextLine();
+		
 		
 		
 	}
@@ -19,25 +35,19 @@ public class Ch3Battle {
 		System.out.println("You chose " + pokechosen + "!");
 		System.out.println("It's a Pokemon batle between " + pokechosen + " and " + poke +"! Go!");
 		System.out.println();
-		userInput.close();
 	}
 	
-	public static void damage() {
+/* Damage method prompts user for base stats and reports damage done to Pokemon. */	
+	public static void damage(int level, int attack, int defense, int base, int hp) {
 		System.out.println("Piplup used Whirpool!");
-		Scanner s= new Scanner(System.in);
-		System.out.print("Trainer, what are your Pokemon's stats?");
-		System.out.print("Level: ");
-		int level= s.nextInt();
-		System.out.print("Attack: ");
-		int attack= s.nextInt();
-		System.out.print("Defense: ");
-		int defense= s.nextInt();
-		System.out.print("Base: ");
-		int base= s.nextInt();
-		System.out.print("HP: ");
-		int hp= s.nextInt();
-		double modifier= Math.random();
+		double modifier= Math.random()*50;
 		int damage= (((2*level*10/250)*(attack/defense)*base)+2)*(int)modifier;
+		System.out.print("Your pokemon sustained " + damage + " points damage.");
+		System.out.println("HP are now " + (hp-damage) + ".");
+	}
+	
+	public static void statsTable(String moves, int level, int attack, int defense, int base, int hp) {
+		System.out.print("Pokemon Stats: ");
 		
 	}
 	
